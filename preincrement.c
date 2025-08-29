d75d16c31b785e456b5ccf8157dee4d33c7f7f47
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-29 23:31:53 +0530

    Create preincrement.c

---
+++ preincrement.c
@@ -0,0 +1,12 @@
+#include<stdio.h>                 // 26
+
+int main() {
+    // increment operator
+    // ++i
+
+    int i = 1;
+    printf("%d\n", ++i);// increase then ,  use
+    printf("%d\n",i);
+
+    return 0;
+}
