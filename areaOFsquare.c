ca60f91c4c522cb878c7a5fb081a845fea19f32d
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-29 22:57:40 +0530

    Create areaOFsquare.c

---
+++ areaOFsquare.c
@@ -0,0 +1,12 @@
+#include<stdio.h>            // 5
+ 
+int main() {
+    int side;
+
+    printf("enter value of side :");
+    scanf("%d", &side);
+
+    printf("Area of square is %d\n",side * side);
+
+    return 0;
+}
