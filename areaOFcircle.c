d0ef520f03d2aeb55cbe9bd75fe0f768a8807765
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-29 22:58:49 +0530

    Create areaOFcircle.c

---
+++ areaOFcircle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>     // 6
+
+int main() {
+    float radius;
+
+    printf("enter value of radius :");
+    scanf("%f", &radius);
+
+    printf("Area of circle is %f\n",3.14 * radius * radius);
+
+    return 0;
+}
