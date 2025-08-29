86dcd4c2b45325c4dbd290276a7d49ac43136a6d
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-29 23:29:29 +0530

    Create postincrement.c

---
+++ postincrement.c
@@ -0,0 +1,12 @@
+#include<stdio.h>           // 25
+
+int main() {
+    // increment operator
+    // i++ 
+
+    int i = 1;
+    printf("%d \n",i++);     //use then , increase 
+    printf("%d\n",i);
+
+    return 0;
+}
