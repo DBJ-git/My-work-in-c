49c07a68669bcc4f506c5178a1d67b54a547ef8f
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-29 22:54:09 +0530

    Create age.c

---
+++ age.c
@@ -0,0 +1,12 @@
+#include<stdio.h>               // 3
+
+int main() {
+     int age;
+   
+   
+    printf("enter age : ");
+    scanf("%d", &age);
+    printf("your age is %d\n",age);
+
+    return 0;
+}
