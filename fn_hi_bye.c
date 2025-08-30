22cfc90e61ceeeb6648b035a85fd40b8ff51d6f4
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:18:02 +0530

    Create fn_hi_bye.c

---
+++ fn_hi_bye.c
@@ -0,0 +1,19 @@
+#include<stdio.h>   // 42
+
+void printHello();
+void printBye();
+
+int main() {
+
+     printHello();
+     printBye();
+    return 0;
+}
+
+void printHello() {
+    printf("Hello!\n");
+}
+
+void printBye() {
+    printf("Good Bye!\n");
+}
