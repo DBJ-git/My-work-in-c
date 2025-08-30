367260057a36feeae81ccc5a63e4d484da1c4224
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:38:16 +0530

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>    // 55
+
+int fib(int n);
+
+int main() {
+    int n;
+
+    printf("Enter a number :");
+    scanf("%d", &n);
+
+    printf("Fibonacci of %d is %d.\n",n,fib(n));
+    return 0;
+}
+
+int fib(int n){         
+    if(n == 0){            
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    int fibNm1 = fib(n - 1);
+    int fibNm2 = fib(n - 2);
+    int fibN = fibNm1 + fibNm2;
+    return fibN;
+}
