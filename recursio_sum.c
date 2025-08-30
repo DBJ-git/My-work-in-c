7e48ea43a9d98abe061a9dd5087c2b4040fbbbfb
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:30:00 +0530

    Create recursio_sum.c

---
+++ recursio_sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>    // 50
+
+int sum(int n);
+
+int  main() {
+
+    printf("Your sum is %d.\n",sum(5));
+
+    return 0;
+}
+
+int sum(int n) {
+    if(n == 1){
+        return 1;
+    }
+    int sumNm1 = sum (n - 1);
+    int sumN = sumNm1 + n;
+    return sumN;
+}
