e765c4f370689b05195b07204a86979943f47348
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:24:49 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>      // 47
+#include<math.h>
+
+int main() {
+    int a;
+
+    printf("Enter a number :");
+    scanf("%d", &a);
+
+    printf("square of number is %f",pow(a,2));
+
+    return 0;
+}
