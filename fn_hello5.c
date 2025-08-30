23d9e0dcf80c02e14d690125618f789f59137152
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:27:52 +0530

    Create fn_hello5.c

---
+++ fn_hello5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>   // 49
+
+void printHello(int count);
+
+int main() {
+
+    printHello(5);
+
+    return 0;
+}
+
+void printHello(int count) {
+    if(count == 0){
+        return;
+    }
+    printf("Hello World.\n");
+    printHello(count - 1);
+}
