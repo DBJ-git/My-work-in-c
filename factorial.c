baf4d5557b04379bbfd364e1c80dad7c1af43c2c
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:13:14 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>      // 38
+#include<math.h>                     
+
+int main() {
+    int n;
+    int fact = 1;
+
+    printf("Enter a number :");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n ; i++) {
+        fact = fact * i;
+    }
+
+    printf("factorial is %d\n",fact);
+
+    return 0;
+}
