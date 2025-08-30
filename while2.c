32db9ef677d8c9d2937a02e1cfc80404c123ea0e
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:02:35 +0530

    Create while2.c

---
+++ while2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>             // 31
+
+int main() {
+    int n;
+
+    printf("Enter a number : ");
+    scanf("%d", &n);
+
+int i = 0; 
+
+    while(i <= n) {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
