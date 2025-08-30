ef05d8b43efb40705f1e61919cf5fbe0381153fe
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:10:14 +0530

    Create input_even.c

---
+++ input_even.c
@@ -0,0 +1,20 @@
+#include<stdio.h>      // 35
+
+int main() {
+    int n;
+
+do {
+    printf("Enter number :");
+    scanf("%d", &n);
+
+    printf("%d\n",n);
+
+    if(n % 2 != 0) {
+        break;
+    }
+} while(1);
+
+printf("Thank you.\n"); 
+
+    return 0;
+}
