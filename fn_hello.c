1c8179a71b71ac194356a7a9adb39b382248f999
DBJ-git <divyjoshi.9999@gmail.com>
2025-08-30 22:16:28 +0530

    Create fn_hello.c

---
+++ fn_hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>     // 41
+
+void printHello(); // function declaration
+
+int main() {
+
+    printHello(); // function call
+   
+    return 0;
+}
+
+void printHello() {
+     printf("Hello World.\n"); // function defination
+}
