88e9bd813c810989417f840457827a14b6c60f6a
DBJ-git <divyjoshi.9999@gmail.com>
2025-09-01 23:34:45 +0530

    Create name_string.c

---
+++ name_string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    char name[100];
+
+    printf("Enter yuor name : ");
+    scanf("%s",name);
+    printf("\n");
+
+    printf("%s\n",name);
+
+    return 0;
+}
