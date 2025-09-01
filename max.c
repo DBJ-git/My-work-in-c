52b9bf729890984fc7020644777139028869753f
DBJ-git <divyjoshi.9999@gmail.com>
2025-09-01 22:48:41 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+
+    printf("Enter first number : ");
+    scanf("%d", &a);
+
+    printf("Enter second number : ");
+    scanf("%d", &b);
+
+    int Maximum;
+
+    if (a >= b )
+       maximum = a;
+    else 
+    maximum = b;
+
+    printf("Maximum number is %d", maximum);
+
+    return 0;
+}
